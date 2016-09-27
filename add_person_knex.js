86116c3e743dc4560422262bb80df607df68f4f7
Amna Zletni <azletni13@gmail.com>
2016-09-27 17:33:50 +0000

    adds file to insert into table with knex

---
+++ add_person_knex.js
@@ -0,0 +1,28 @@
+const settings = require("./settings"); // settings.json
+const pg = require("pg");
+
+const knex = require("knex")({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+    ssl      : settings.ssl
+  }
+});
+
+// var firstName = process.argv[2];
+// var lastName = process.argv[3];
+// var birthDate = process.argv[4];
+
+knex.insert([{first_name: 'Amna', last_name: 'Zletni', birthdate: '1991-12-13'}]).into('famous_people')
+.finally(function() {
+knex.destroy();
+});
+
+// knex.('famous_people').insert([{first_name: firstName}, {last_name: lastName}, {birthdate: birthDate}])
+// .finally(function() {
+// knex.destroy();
+// });
